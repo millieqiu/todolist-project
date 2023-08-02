e6fc308f813aa9da9b5e4f3e832fe1c17813bc5d
QiuMillie <113364664+Qiumodoo1110@users.noreply.github.com>
2023-08-02 18:34:52 +0800

    feat: 新增網頁

---
+++ todoAPP/todoAPP/Pages/TodoPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace todoAPP.Pages
+{
+    public class TodoPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
