6c52a62af3da17713068c4070201e8c3e0593f6a
vincent87720 <vincent87720@yahoo.com.tw>
2023-08-04 12:01:28 +0800

    Feat: Get and display todo data in TodoPage

--- todoAPP/todoAPP/wwwroot/js/site.js
+++ todoAPP/todoAPP/wwwroot/js/site.js
@@ -1,5 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function getTodoList() {
+    $.ajax({
+        url: "/api/TodoList/ListAll",
+        method: "get",
+        success: function (res) {
+            $.each(res, function (key, value) {
+                $('#todolist').append(`<div class="task">
+                                            <label class="task-content">
+                                                <input type="checkbox" name="" id="${value.id}" class="checkbox" onclick="onChangeStatus(${value.id})">
+                                                <span class="gray-01 paragraph1" for="">${value.text}</span>
+                                            </label>
+                                            <div class="icon">
+                                                <i class="fa-solid fa-trash gray-04"></i>
+                                            </div>
+                                        </div>`).html();
 
-// Write your JavaScript code.
-
+                return res;
+            });
+        }
+    })
+}
