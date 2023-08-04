524c314e13f6cfa9e1a34384400059afd8ffdc37
vincent87720 <vincent87720@yahoo.com.tw>
2023-08-04 14:32:06 +0800

    Fix: the checkbox status error

--- todoAPP/todoAPP/wwwroot/js/todoList.js
+++ todoAPP/todoAPP/wwwroot/js/todoList.js
@@ -13,9 +13,13 @@ function getTodoList() {
         method: "get",
         success: function (res) {
             $.each(res, function (key, value) {
+                let status = "";
+                if (value.status == 1) {
+                    status = "checked";
+                }
                 $('#todolist').append(`<div class="task">
                                             <label class="task-content">
-                                                <input type="checkbox" name="" id="${value.id}" class="checkbox" onclick="onChangeStatus(${value.id})">
+                                                <input type="checkbox" name="" id="checkbox_item_${value.id}" class="checkbox" onclick="onChangeStatus(${value.id})" ${status}>
                                                 <span class="gray-01 paragraph1" for="">${value.text}</span>
                                             </label>
                                             <div class="icon">
