3182915fac4ca26f82c44704ebfce93f60d849de
vincent87720 <vincent87720@yahoo.com.tw>
2023-08-07 01:52:15 +0800

    Fix: error handling

--- todoAPP/todoAPP/wwwroot/js/site.js
+++ todoAPP/todoAPP/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-function onClickLoginOrRegister(event) {
+﻿function onClickLoginOrRegister(event) {
     event.preventDefault();
     const loginEmail = document.getElementById('loginEmail').value;
     const loginPassword = document.getElementById('loginPassword').value;
@@ -34,7 +34,14 @@ function login(username, password) {
         },
         error: function (req, status) {
             if (req.responseJSON.service == "Login" && req.responseJSON.status == 1) {
-                window.location.replace("/Register");
+                alert("帳號不存在，為您跳轉至註冊頁面進行註冊");
+                window.location.assign("/Register");
+            }
+            else if (req.responseJSON.service == "Login" && req.responseJSON.status == 2) {
+                alert("帳號或密碼錯誤");
+            }
+            else {
+                alert("無法登入，請聯絡系統管理員");
             }
         }
     })
@@ -45,7 +52,10 @@ function logout() {
         url: "/api/User/Logout",
         method: "post",
         success: function (res) {
-            window.location.replace("/Index");
+            window.location.assign("/Index");
+        },
+        error: function (req, status) {
+            alert("登出失敗，請重新嘗試登出");
         }
     })
 }
@@ -67,8 +77,17 @@ function register(username, password, confirmPassword, nickname) {
             window.location.assign("/Index");
         },
         error: function (req, status) {
-            if (req.responseJSON.service == "Register" && req.responseJSON.status == 2) {
-                alert("密碼與確認密碼不符合")
+            if (req.responseJSON.service && req.responseJSON.service == "Register" && req.responseJSON.status == 1) {
+                alert("帳號已存在");
+            }
+            else if (req.responseJSON.errors.Username && req.responseJSON.errors.Username.includes("The Username field is not a valid e-mail address.")) {
+                alert("Email格式錯誤");
+            }
+            else if (req.responseJSON.errors.ConfirmPassword) {
+                alert("密碼與確認密碼不符合");
+            }
+            else {
+                alert("無法註冊帳號，請聯絡系統管理員");
             }
         }
     })
--- todoAPP/todoAPP/wwwroot/js/todoList.js
+++ todoAPP/todoAPP/wwwroot/js/todoList.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+﻿window.onload = function () {
     getUserInfo();
     getTodoList();
 }
