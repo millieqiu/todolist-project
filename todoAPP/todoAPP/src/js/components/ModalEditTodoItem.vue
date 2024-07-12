5b8929077c8707932036b65bd92694681272ffa9
millieqiu <millie.yi110@gmail.com>
2024-07-12 18:00:29 +0800

    Feat: update appsettings

--- todoAPP/todoAPP/src/js/components/ModalEditTodoItem.vue
+++ todoAPP/todoAPP/src/js/components/ModalEditTodoItem.vue
@@ -15,6 +15,12 @@
               d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
           </svg>
         </label>
+        <select class="select select-bordered w-full">
+          <option disabled selected>待辦事項標籤</option>
+          <option>Normal Apple</option>
+          <option>Normal Orange</option>
+          <option>Normal Tomato</option>
+        </select>
         <textarea class="textarea textarea-bordered" rows="5" placeholder="備註及描述" v-model="todoForm.note"></textarea>
       </div>
       <div class="modal-action">
--- todoAPP/todoAPP/src/js/components/ModalSetting.vue
+++ todoAPP/todoAPP/src/js/components/ModalSetting.vue
@@ -3,25 +3,54 @@
     <div class="modal-box w-11/12 max-w-5xl p-0">
       <h3 class="font-bold text-lg p-4">設定</h3>
       <form method="dialog">
-        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          ✕
+        </button>
       </form>
       <div class="modal-body border-t-2 border-base-200">
         <ul class="menu bg-base-200 w-56">
           <li><a>個人資料</a></li>
         </ul>
-        <div class="p-2">
-          <div class="flex flex-col gap-2">
+        <div class="p-2 flex-1" v-if="!isChangingPassword">
+          <div class="flex flex-col gap-4">
+            <div>
+              <h4 class="font-medium text-sm p-1">頭像</h4>
+              <div class="flex items-center">
+                <div class="avatar">
+                  <div class="w-24 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                  </div>
+                </div>
+                <button class="btn ml-3">變更頭像</button>
+              </div>
+            </div>
             <div>
               <h4 class="font-medium text-sm p-1">電子信箱</h4>
               <span class="text-sm">aaabbb@gmail.com</span>
             </div>
             <div>
               <h4 class="font-medium text-sm p-1">密碼</h4>
-              <span class="text-sm">aaabbb@gmail.com</span>
+              <button class="btn" @click="isChangingPassword = true">變更密碼</button>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 flex-1" v-if="isChangingPassword">
+          <div class="flex flex-col gap-4">
+            <div>
+              <h4 class="font-medium text-sm p-1">舊密碼</h4>
+              <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" />
             </div>
             <div>
-              <h4 class="font-medium text-sm p-1">密碼</h4>
-              <span class="text-sm">aaabbb@gmail.com</span>
+              <h4 class="font-medium text-sm p-1">新密碼</h4>
+              <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" />
+            </div>
+            <div>
+              <h4 class="font-medium text-sm p-1">確認新密碼</h4>
+              <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" />
+            </div>
+            <div class="mt-4">
+              <button class="btn mr-3">返回</button>
+              <button class="btn btn-primary">確認變更</button>
             </div>
           </div>
         </div>
@@ -32,10 +61,12 @@
 
 <script>
 // # Vue
-import { onMounted, reactive, ref } from 'vue';
+import { onMounted, reactive, ref } from "vue";
 
 export default {
   setup(props, { emit }) {
+
+    const isChangingPassword = ref(false);
 
     const modalEl = ref(null);
 
@@ -51,9 +82,10 @@ export default {
       modalEl,
       openModal,
       closeModal,
-    }
-  }
-}
+      isChangingPassword
+    };
+  },
+};
 </script>
 
 <style scoped>
